ae137cb01420a3708a1b9e817b7ceb59d161be5f
MMureed-Hussain <malikmureedh@gmail.com>
2023-09-15 16:50:56 +0500

    Create Footer Page

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Sidebar from "../Sidebar";
-import Navbar from "../Navbar";
-import Header from "./Header";
 import Help from "../Help";
+import { Footer } from "../Footer";
 export default function Layout({ children }) {
   const location = useLocation();
   const is404Page = location.pathname === "/404";
@@ -19,6 +18,7 @@ export default function Layout({ children }) {
           <div className="col-10">{children}</div>
           <Help/>
         </div>
+        <Footer/>
     </>
   );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,55 +1,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Footer } from "../components/Footer";
 
 const LandingPage = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <Link to="/" className="navbar-brand">
-              Home
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link to="/registration" className="nav-link">
-                    Register
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/login" className="nav-link">
-                    Login
-                  </Link>
-                </li>
-              </ul>
+    <>
+      <div className="container">
+        <div className="row">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+              <Link to="/" className="navbar-brand">
+                Home
+              </Link>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link to="/registration" className="nav-link">
+                      Register
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/login" className="nav-link">
+                      Login
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
+          </nav>
+          <div className="col-md-6 mt-5 text-center">
+            <h2>Welcome to Our Project</h2>
+            <p>Project details and information go here.</p>
           </div>
-        </nav>
-        <div className="col-md-6 mt-5 text-center">
-          <h2>Welcome to Our Project</h2>
-          <p>Project details and information go here.</p>
-        </div>
-        <div className="col-md-6 mt-5 mb-5">
-          <img
-            src="/images/landing-page.jpg"
-            alt="Landing Image"
-            className="img-fluid"
-          />
+          <div className="col-md-6 mt-5 mb-5">
+            <img
+              src="/images/landing-page.jpg"
+              alt="Landing Image"
+              className="img-fluid"
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
